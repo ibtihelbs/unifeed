4465fd4a2104edded5b91898871382b441f73f91
ibtihelbs <ibtihel.bensalah@outlook.fr>
2023-01-04 08:58:15 +0100

    add task_1

---
+++ app.js
@@ -0,0 +1,7 @@
+console.log('hfdhfiudhfiu')
+const header = document.querySelector("header")
+
+header.innerHTML = `<ul>
+<li><a href="index.html">DEMO</a></li>
+<li><a href="tasks.html">Tasks</a></li>
+</ul>`;
