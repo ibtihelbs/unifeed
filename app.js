de55acf280360e3a09373dfe5585a0488713383c
ibtihelbs <ibtihel.bensalah@outlook.fr>
2023-01-06 14:38:42 +0100

    add tasks

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ const header = document.querySelector("header")
 
 header.innerHTML = `<ul>
 <li><a href="index.html">DEMO</a></li>
-<li><a href="tasks.html">Tasks</a></li>
+<li><a href="tasks/task_3.html">Tasks</a></li>
 </ul>`;
